17c178bc7d51e61699611500dba24a7abed79ce4
imrany <imranmat254@gmail.com>
2023-10-22 22:05:26 +0300

    fetching data from a json file

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,160 +1,16 @@
 /** @type {import('./$types').PageLoad} */
 export async function load({ fetch }) {
     try{
-        // const url=`https://jsonplaceholder.typicode.com/users`
-        // const response =await fetch(url);
-        // const data= await response.json();
+        const url=`/data/food.json`
+        const response =await fetch(url);
+        const data= await response.json();
 
-        // if(response.ok){
-        //         return {data}
-        // }else{
-        //     return {
-        //     error:`Status ${response.status}, Could not fetch!`
-        //     }
-        // }
-        return {
-            data:[
-                {
-                    id:1,
-                    title:'Pineapple',
-                    image:'/pineapple.png',
-                    kcal:'294 kcal | 200G',
-                    measurements:[
-                        {
-                            quantity:'27g',
-                            type:"Proteins"
-                        },
-                        {
-                            quantity:'8g',
-                            type:"Carbs"
-                        },
-                        {
-                            quantity:'27g',
-                            type:"Fats"
-                        }
-                    ],
-                },
-                {
-                    id:2,
-                    title:'Porridge',
-                    image:'/porridge.png',
-                    kcal:'294 kcal | 200G',
-                    measurements:[
-                        {
-                            quantity:'27g',
-                            type:"Proteins"
-                        },
-                        {
-                            quantity:'8g',
-                            type:"Carbs"
-                        },
-                        {
-                            quantity:'27g',
-                            type:"Fats"
-                        }
-                    ],
-                },
-                {
-                    id:3,
-                    title:'Brocolli',
-                    image:'/brocolli.png',
-                    kcal:'294 kcal | 200G',
-                    measurements:[
-                        {
-                            quantity:'27g',
-                            type:"Proteins"
-                        },
-                        {
-                            quantity:'8g',
-                            type:"Carbs"
-                        },
-                        {
-                            quantity:'27g',
-                            type:"Fats"
-                        }
-                    ],
-                },
-                {
-                    id:4,
-                    title:'Apple',
-                    image:'/apple.png',
-                    kcal:'294 kcal | 200G',
-                    measurements:[
-                        {
-                            quantity:'27g',
-                            type:"Proteins"
-                        },
-                        {
-                            quantity:'8g',
-                            type:"Carbs"
-                        },
-                        {
-                            quantity:'27g',
-                            type:"Fats"
-                        }
-                    ],
-                },
-                {
-                    id:5,
-                    title:'Beans',
-                    image:'/beans.png',
-                    kcal:'294 kcal | 200G',
-                    measurements:[
-                        {
-                            quantity:'27g',
-                            type:"Proteins"
-                        },
-                        {
-                            quantity:'8g',
-                            type:"Carbs"
-                        },
-                        {
-                            quantity:'27g',
-                            type:"Fats"
-                        }
-                    ],
-                },
-                {
-                    id:6,
-                    title:'Eggs',
-                    image:'/eggs.png',
-                    kcal:'294 kcal | 200G',
-                    measurements:[
-                        {
-                            quantity:'27g',
-                            type:"Proteins"
-                        },
-                        {
-                            quantity:'8g',
-                            type:"Carbs"
-                        },
-                        {
-                            quantity:'27g',
-                            type:"Fats"
-                        }
-                    ],
-                },
-                {
-                    id:7,
-                    title:'Rice',
-                    image:'/rice.png',
-                    kcal:'294 kcal | 200G',
-                    measurements:[
-                        {
-                            quantity:'27g',
-                            type:"Proteins"
-                        },
-                        {
-                            quantity:'8g',
-                            type:"Carbs"
-                        },
-                        {
-                            quantity:'27g',
-                            type:"Fats"
-                        }
-                    ],
-                },
-            ]
+        if(response.ok){
+                return {data}
+        }else{
+            return {
+            error:`Status ${response.status}, Could not fetch!`
+            }
         }
     }catch(error){
         return {
